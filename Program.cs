6dd517ca69ec4fa9f233b06df75bb24f185d53ac
kaapo <kaapo.pikkarainen@gmail.com>
2025-12-11 15:48:08 +0200

    hard code, hard code, hardcore

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Net;
+using System.Security.Cryptography.X509Certificates;
+using Blaze2SDK;
+using Blaze3SDK;
+using NLog;
+using NLog.Layouts;
+using LogLevel = NLog.LogLevel;
+
+namespace TheDirector;
+
+internal class Program
+{
+    public const string Name = "TheDirector 1.0";
+
+    private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+    private static async Task Main(string[] args)
+    {
+        StartLogger();
+        
+        var redirectorTaskSdk2 = StartRedirectorServerSdk2();
+        var redirectorTaskSdk3 = StartRedirectorServerSdk3();
+        
+        Logger.Warn(Name + " started");
+        await Task.WhenAll(redirectorTaskSdk2, redirectorTaskSdk3);
+    }
+
+    private static void StartLogger()
+    {
+        var logLevel = LogLevel.Trace;
+        var layout = new SimpleLayout("[${longdate}][${callsite-filename:includeSourcePath=false}(${callsite-linenumber})][${level:uppercase=true}]: ${message:withexception=true}");
+        LogManager.Setup().LoadConfiguration(builder =>
+        {
+            builder.ForLogger().FilterMinLevel(logLevel)
+                .WriteToConsole(layout)
+                .WriteToFile("logs/server-${shortdate}.log", layout);
+        });
+    }
+
+    private static async Task StartRedirectorServerSdk2()
+    {
+        var redirector = Blaze2.CreateBlazeServer("RedirectorServer", new IPEndPoint(IPAddress.Any, 42100));
+        redirector.AddComponent<RedirectorComponent>();
+        await redirector.Start(-1).ConfigureAwait(false);
+    }
+    
+    private static async Task StartRedirectorServerSdk3()
+    {
+        var certBytes = File.ReadAllBytes("gosredirector_mod.pfx");
+        X509Certificate cert = new X509Certificate2(certBytes, "123456");
+
+        var redirector = Blaze3.CreateBlazeServer("RedirectorServer", new IPEndPoint(IPAddress.Any, 42127), cert);
+        redirector.AddComponent<RedirectorComponent>();
+        await redirector.Start(-1).ConfigureAwait(false);
+    }
+    
+}
---
+++ RedirectorComponent.cs
@@ -0,0 +1,61 @@
+using Blaze3SDK.Blaze.Redirector;
+using Blaze3SDK.Components;
+using BlazeCommon;
+
+namespace TheDirector;
+
+internal class RedirectorComponent : RedirectorComponentBase.Server
+{
+    private static readonly string PublicIp = new HttpClient().GetStringAsync("https://checkip.amazonaws.com/").GetAwaiter().GetResult().Trim();
+
+    public override Task<ServerInstanceInfo> GetServerInstanceAsync(ServerInstanceRequest request, BlazeRpcContext context)
+    {
+
+        var responseData = new ServerInstanceInfo
+        {
+            mAddress = new ServerAddress
+            {
+                IpAddress = new IpAddress
+                {
+                    mHostname = PublicIp,
+                    mIp = Util.GetIPAddressAsUInt(PublicIp),
+                },
+            },
+            mMessages = new List<string>
+            {
+                {"You are now connecting to a private server Zamboni\nNot affiliated with EA Sports"}
+            },
+            mSecure = false,
+        };
+        
+        switch (request.mClientName)
+        {
+            case "NHL10":
+            {
+                responseData.mAddress.IpAddress = responseData.mAddress.IpAddress.Value with { mPort = 13337 };
+                break;
+            }
+            case "NHL11":
+            {
+                if (request.mBlazeSDKBuildDate.Equals("Sep 21 2010 18:00:56") || request.mBlazeSDKBuildDate.Equals("Sep 21 2010 18:01:00"))
+                {
+                    responseData.mMessages = new List<string>
+                    {
+                        { "Playing with a title patch (versions 01.01 or 01.02) is not currently supported by this server.\nPlease downgrade your game to play online" }
+                    };
+                    break;
+                }
+                responseData.mAddress.IpAddress = responseData.mAddress.IpAddress.Value with { mPort = 13367 };
+                break;
+            }
+            case "NHL16":
+            {
+                responseData.mAddress.IpAddress = responseData.mAddress.IpAddress.Value with { mPort = 16767 };
+                break;
+            }
+            
+        }
+
+        return Task.FromResult(responseData);
+    }
+}
